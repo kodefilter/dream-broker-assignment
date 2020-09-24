cf5d7c2b583db504cfba554beb2389781487d953
prrdee <18732242+prrdee@users.noreply.github.com>
2020-09-25 01:35:04 +0300

    extra commit

--- dist-server/app.js
+++ dist-server/app.js
@@ -1,9 +1,11 @@
 "use strict";
 
+require("core-js/modules/es.array.join");
+
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports["default"] = void 0;
+exports.default = void 0;
 
 var _express = _interopRequireDefault(require("express"));
 
@@ -17,17 +19,17 @@ var _index = _interopRequireDefault(require("./routes/index"));
 
 var _analyze = _interopRequireDefault(require("./routes/analyze"));
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var app = (0, _express["default"])();
-app.use((0, _morgan["default"])('dev'));
-app.use(_express["default"].json());
-app.use(_express["default"].urlencoded({
+var app = (0, _express.default)();
+app.use((0, _morgan.default)('dev'));
+app.use(_express.default.json());
+app.use(_express.default.urlencoded({
   extended: false
 }));
-app.use((0, _cookieParser["default"])());
-app.use(_express["default"]["static"](_path["default"].join(__dirname, '../public')));
-app.use('/', _index["default"]);
-app.use('/analyze', _analyze["default"]);
+app.use((0, _cookieParser.default)());
+app.use(_express.default.static(_path.default.join(__dirname, '../public')));
+app.use('/', _index.default);
+app.use('/analyze', _analyze.default);
 var _default = app;
-exports["default"] = _default;
+exports.default = _default;
