99cd5bcd55959be3ee7ab2324d51ab4cefa1597f
prrdee <18732242+prrdee@users.noreply.github.com>
2020-09-25 00:39:45 +0300

    Create a function for character count

--- server/services/analyze.js
+++ server/services/analyze.js
@@ -10,4 +10,33 @@ function wordCount(inputText){
     return matches ? matches.length : 0
 }
 
-export default { textLength, wordCount }
+function characterCount(inputText) {
+    //removing non alphabetic characters
+    const cleanString = inputText.toLowerCase().replace(/[^A-Za-z]/g, '')
+  
+    const arrayOfArrays = Object.entries(
+      [...cleanString].reduce((obj, item) => {
+        if (item in obj) {
+          obj[item]++
+        } else {
+          obj[item] = 1
+        }
+        return obj
+      }, {}),
+    )
+  
+    // map array of arrays to array of objects
+    const arrayOfObjects = arrayOfArrays.map(function ([a, b]) {
+      return { [a]: b }
+    })
+  
+    // sort array of objects alphabetically
+    const result = arrayOfObjects.sort(function (a, b) {
+      if (Object.keys(a)[0] > Object.keys(b)[0]) return 1
+      return -1
+    })
+  
+    return result
+}
+
+export default { textLength, wordCount, characterCount }
