46935d59692e15d665aafb8bf94046297e932c8a
prrdee <18732242+prrdee@users.noreply.github.com>
2020-09-25 00:31:44 +0300

    Add unit tests for word count function

---
+++ server/__tests__/word_count.spec.js
@@ -0,0 +1,15 @@
+import AnalyzeService from '../services/analyze'
+
+describe("wordCount", () => {
+    it("should return 3 for hello hi hei", () => {
+        expect(AnalyzeService.wordCount("hello hi hei")).toEqual(3)
+    });
+
+    it("should return 1 for hello with trailing 2 spaces", () => {
+        expect(AnalyzeService.wordCount("hello  ")).toEqual(1)
+    });
+
+    it("should return 0 for empty string", () => {
+        expect(AnalyzeService.wordCount("")).toEqual(0)
+    });
+});
