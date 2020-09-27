2d93d86eb09885953545d8741bac8188f1098f6b
prrdee <18732242+prrdee@users.noreply.github.com>
2020-09-27 16:48:56 +0300

    Create Middleware for content type error handling

--- server/middlewares/apiContentType.js
+++ server/middlewares/apiContentType.js
@@ -2,7 +2,7 @@ import { BadRequestError } from '../helpers/apiError'
 
 export default function(req, res, next) {
   if ((req.method === 'POST') && !req.is('application/json')) {
-    next(new BadRequestError())
+    next(new BadRequestError('Wrong Content Type'))
   } else {
     next()
   }
