61bb7baf62891a575b05424ee6f76164ebe0c00c
prrdee <18732242+prrdee@users.noreply.github.com>
2020-09-27 16:50:20 +0300

    Catch errors

--- dist-server/app.js
+++ dist-server/app.js
@@ -1,5 +1,7 @@
 "use strict";
 
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
 require("core-js/modules/es.array.join");
 
 Object.defineProperty(exports, "__esModule", {
@@ -7,7 +9,7 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 
-var _express = _interopRequireDefault(require("express"));
+var _express = _interopRequireWildcard(require("express"));
 
 var _path = _interopRequireDefault(require("path"));
 
@@ -25,6 +27,10 @@ var _apiContentType = _interopRequireDefault(require("./middlewares/apiContentTy
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function _getRequireWildcardCache() { return cache; }; return cache; }
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+
 var app = (0, _express.default)();
 app.use((0, _morgan.default)('dev'));
 app.use(_express.default.json());
@@ -32,10 +38,12 @@ app.use(_express.default.urlencoded({
   extended: false
 }));
 app.use((0, _cookieParser.default)());
-app.use(_express.default.static(_path.default.join(__dirname, '../public'))); //app.use(apiContentType)
+app.use(_express.default.static(_path.default.join(__dirname, '../public'))); //Custom middleware
 
+app.use(_apiContentType.default);
 app.use('/', _index.default);
-app.use('/analyze', _analyze.default);
+app.use('/analyze', _analyze.default); //Custom middleware
+
 app.use(_apiErrorHandler.default);
 var _default = app;
 exports.default = _default;
--- server/routes/analyze.js
+++ server/routes/analyze.js
@@ -1,27 +1,19 @@
 import express from 'express'
+import { BadRequestError, InternalServerError } from '../helpers/apiError'
 var router = express.Router()
 
 import AnalyzeService from '../services/analyze'
 
-import { BadRequestError, InternalServerError } from '../helpers/apiError'
-
 /* POST to /analyze */
 router.post('/', function (req, res, next) {
-  
   try {
     const { text } = req.body
     const result = AnalyzeService.analyzeText(text)
     res.json(result)
   } catch (error) {
-    
     if (error.name === 'TypeError') {
-      
-      console.log('Indside Analyze Bad Request',`\x1B[31m${error}`)
-
-      next(new BadRequestError('Invalid Request',error))
+      next(new BadRequestError('Invalid Request', error))
     } else {
-      console.log('Indside Analyze',`\x1B[31m${error}`)
-
       next(new InternalServerError('Internal Server Error', error))
     }
   }
