0a0cec49d94b08f36bb01b74c24cbac6b2556837
prrdee <18732242+prrdee@users.noreply.github.com>
2020-09-27 17:20:54 +0300

    Remove unused packages

--- dist-server/app.js
+++ dist-server/app.js
@@ -13,10 +13,6 @@ var _express = _interopRequireWildcard(require("express"));
 
 var _path = _interopRequireDefault(require("path"));
 
-var _cookieParser = _interopRequireDefault(require("cookie-parser"));
-
-var _morgan = _interopRequireDefault(require("morgan"));
-
 var _index = _interopRequireDefault(require("./routes/index"));
 
 var _analyze = _interopRequireDefault(require("./routes/analyze"));
@@ -32,12 +28,10 @@ function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
 var app = (0, _express.default)();
-app.use((0, _morgan.default)('dev'));
 app.use(_express.default.json());
 app.use(_express.default.urlencoded({
   extended: false
 }));
-app.use((0, _cookieParser.default)());
 app.use(_express.default.static(_path.default.join(__dirname, '../public'))); //Custom middleware
 
 app.use(_apiContentType.default);
--- server/app.js
+++ server/app.js
@@ -1,16 +1,12 @@
 import express, { response } from 'express';
 import path from 'path';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
 import indexRouter from './routes/index';
 import analyzeRouter from './routes/analyze';
 import apiErrorHandler from './middlewares/apiErrorHandler'
 import apiContentType from './middlewares/apiContentType';
 var app = express();
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 //Custom middleware
 app.use(apiContentType)
