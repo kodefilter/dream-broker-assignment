99698c86742c4688ab7058d86fc7cd841e5a9615
prrdee <18732242+prrdee@users.noreply.github.com>
2020-09-27 16:39:15 +0300

    Add tests for wrong content type and invalid request

--- server/__tests__/e2e.spec.js
+++ server/__tests__/e2e.spec.js
@@ -12,13 +12,23 @@ describe('POST request to endpoint /analyze', () => {
         expect(response.body).toEqual({"characterCount": [{"e": 2}, {"h": 3}, {"i": 2}, {"l": 2}, {"o": 1}], "textLength": {"withSpaces": 12, "withoutSpaces": 10}, "wordCount": 3})
     })
 
-    it('should resond with error if wrong data is sent', async () => {
+    it('should resond with 400 if data is sent with Content-Type other than application/json', async () => {
         const response = await request(app)
         .post('/analyze')
-        .send({
-            wrong : 'data'
-        })
+        .set('Content-Type', 'text/plain')
+        .send('{ text : "hello hi hei" }')
         expect(response.statusCode).toEqual(400)
+        expect(response.body.message).toEqual('Wrong Content Type')
     })
+
+    it('should resond with 400 Invalid Request if wrong data is sent', async () => {
+        const response = await request(app)
+        .post('/analyze')
+        .send({ nottext : "hello hi hei" })
+        expect(response.statusCode).toEqual(400)
+        expect(response.body.message).toEqual('Invalid Request')
+    })
+
+
 
 })
