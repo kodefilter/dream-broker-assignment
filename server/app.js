3ca2134715aa826c64cd7db03c84d5aca15439d6
prrdee <18732242+prrdee@users.noreply.github.com>
2020-09-28 21:55:56 +0300

    Use cors

--- server/app.js
+++ server/app.js
@@ -1,18 +1,18 @@
-import express from 'express';
-import path from 'path';
-import indexRouter from './routes/index';
-import analyzeRouter from './routes/analyze';
+import express from 'express'
+import path from 'path'
+import cors from 'cors'
+import indexRouter from './routes/index'
+import analyzeRouter from './routes/analyze'
 import apiErrorHandler from './middlewares/apiErrorHandler'
-var app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, '../public')));
-
-app.use('/', indexRouter);
-app.use('/analyze', analyzeRouter);
+var app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, '../public')))
+app.use(cors())
+app.use('/', indexRouter)
+app.use('/analyze', analyzeRouter)
 
 //Custom middleware
 app.use(apiErrorHandler)
 
-
-export default app;
+export default app
